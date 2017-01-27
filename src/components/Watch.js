2dff583b61100a98c234751e4f79a78c36fc3eae
brittshroyer <britt.shroyer@gmail.com>
2017-01-27 10:06:06 -0600

    added youtube api key

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -4,7 +4,7 @@ class Watch extends Component{
   render(){
     return(
       <div>
-        <h2>Internet Bangers</h2>
+        <h2>Internet Favorites</h2>
         <div className="antenna"></div>
         <div className="top"></div>
         <div className="tv"></div>
